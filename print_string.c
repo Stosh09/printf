d37fbdb656c1d7e9c73751ec73029e655c3c6b8f
Stosh09 <stoshochieng@gmail.com>
2023-06-24 17:51:07 +0300

    String and character conversions

--- print_string.c
+++ print_string.c
@@ -1,38 +1,56 @@
 #include "main.h"
 
 /**
- * print_cstring - prints a string customised for non printable characters
- * @parg: pointer to variable argument list
- * Return: number of characters printed
+ * print_string - Prints a string
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int print_cstring(va_list parg)
+int print_string(va_list types, char buffer[],
+	int flags,int width,int precision,int size)
 {
-char *s = va_arg(parg, char *);
-int i = 0;
-int count = 0;
-unsigned int xter = 0;
+int length = 0, i;
+char *str = va_arg(types, char *);
 
-if (s == NULL)
-count += _printf("%s", "(null)");
-else
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
+if (str == NULL)
 {
-while (s[i] != '\0')
+str = "(null)";
+if (precision >= 6)
+str = "      ";
+}
+
+while (str[length] != '\0')
+length++;
+
+if (precision >= 0 && precision < length)
+length = precision;
+
+if (width > length)
 {
-if (s[i] < 32 || s[i] >= 127)
+if (flags & F_MINUS)
 {
-xter = s[i];
-putchar('\\');
-putchar('x');
-count += 2;
-count += (xter < 16) ? _printf("0%X", xter) : _printf("%X", xter);
+write(1, &str[0], length);
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+return (width);
 }
 else
 {
-putchar(s[i]);
-count++;
+for (i = width - length; i > 0; i--)
+write(1, " ", 1);
+write(1, &str[0], length);
+return (width);
 }
-i++;
 }
-}
-return (count);
+
+return (write(1, str, length));
 }
